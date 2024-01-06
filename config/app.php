d5b13ec7f5643f98f0b01f7acc086376353ab8c4
Thiarson <thiarsonantsa00@gmail.com>
2024-01-06 21:22:22 +0300

    Create configuration that contains path and information about the environnement of the project

---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php
+
+  // Here we add the absolute path in our project.
+
+  return [
+    // The root directory of our project.
+    'rootDir' => dirname(__DIR__),
+
+    // The path to the folder views.
+    'viewsPath' => dirname(__DIR__).'/resources/views',
+  ];
