0b6b8af86ad91248dd8b07a69127a80ea0c754d6
Thiarson <thiarsonantsa00@gmail.com>
2024-01-06 21:28:56 +0300

    Create base controller that every controller will extends

---
+++ core/Controller/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+  namespace Thiarson\Framework\Controller;
+
+  use Thiarson\Framework\View\View;
+
+  class Controller {
+    /**
+     * Implement the render method based on the view render, so every controller that extends this base controller will have this method.
+     * 
+     * @param $view
+     * @param array $params
+     * @param string $layout
+     */
+    public function render($view, array $params = [], string $layout = 'default') {
+      $views = new View($layout);
+      echo $views->render($view, $params);
+    }
+  }
