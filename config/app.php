9bf3e439135d514a1dc5754ef527636e3a673e30
Thiarson <thiarsonantsa00@gmail.com>
2024-01-08 18:41:44 +0300

    Add layouts path in the configuration of the project

--- config/app.php
+++ config/app.php
@@ -8,4 +8,7 @@
 
     // The path to the folder views.
     'viewsPath' => dirname(__DIR__).'/resources/views',
+
+    // The path to the folder layouts.
+    'layoutsPath' => dirname(__DIR__).'/resources/views/layouts',
   ];
