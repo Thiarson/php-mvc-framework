d2617f44ba39da1bdd5b03a52ba164afd0e3aa32
Thiarson <thiarsonantsa00@gmail.com>
2024-01-08 10:27:12 +0300

    Add methods to manage different HTTP methods and to extract data sent in the request

--- core/Http/Request.php
+++ core/Http/Request.php
@@ -20,4 +20,45 @@
     public function getMethod() {
       return $_SERVER['REQUEST_METHOD'];
     }
+
+    /**
+     * Determine if the HTTP method is GET.
+     *
+     * @return  bool  
+     */
+    public function isGet() {
+      return $this->getMethod() === 'GET';
+    }
+
+    /**
+     * Determine if the HTTP method is POST.
+     *
+     * @return  bool  
+     */
+    public function isPost() {
+      return $this->getMethod() === 'POST';
+    }
+
+    /**
+     * Extract all the data sent in the request.
+     *
+     * @return   
+     */
+    public function getBody() {
+      $body = [];
+      
+      if ($this->isGet()) {
+        foreach ($_GET as $key => $value) {
+          $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+        }
+      }
+
+      if ($this->isPost()) {
+        foreach ($_POST as $key => $value) {
+          $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+        }
+      }
+
+      return $body;
+    }
   }
